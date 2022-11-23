6dc33061349e7824234efc8acec211dab409db5b
Elena <elenka81086@mail.ru>
2022-11-23 17:54:46 +0300

    задача 23

---
+++ problem23/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и 
+выдаёт таблицу кубов чисел от 1 до N.*/
+
+
+Console.WriteLine("Введите число: "); 
+int NewNumber = Convert.ToInt32(Console.ReadLine());
+int NamberTheCube = 1;
+while (NamberTheCube <= NewNumber)
+{
+    Console.WriteLine($"Для числа {NamberTheCube} куб составляет {NamberTheCube * NamberTheCube * NamberTheCube + ""}");
+    NamberTheCube++;
+   
+}
